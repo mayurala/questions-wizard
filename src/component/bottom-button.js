318bdc6530ba6be9fbdb4f05f01765be7bec5d04
Abdul Quadir <aquadir83@gmail.com>
2019-07-03 16:45:45 +0530

    Changes to show and hide pervious and submit button.

--- src/component/bottom-button.js
+++ src/component/bottom-button.js
@@ -2,7 +2,7 @@ import { QuestionButtons } from './button';
 
 export default class ActionButton {
     constructor(action, maxValue) {
-        this.buttonLabel = ['Previous', 'Next']
+        this.buttonLabel = ['Previous', 'Next', 'Submit']
         this.currentCount = 0;
         this.willCounterMove = false;
         this.action = action;
@@ -19,6 +19,8 @@ export default class ActionButton {
     getElement() {
         const element = document.createElement('div');
         element.innerHTML = (`${this.buttonLabel.map(item => QuestionButtons(item))}`);
+        element.querySelector('#' + this.buttonLabel[0]).style.display = 'none';
+        element.querySelector('#' + this.buttonLabel[2]).style.display = 'none';
         element.onclick = e => {
             if (!this.willCounterMove && e.target.innerText !== this.buttonLabel[0]) {
                 alert('Please provide the answer before move to next!')
@@ -35,6 +37,12 @@ export default class ActionButton {
             }
             this.currentCount = indx === -1 ? Math.max(this.currentCount + indx, 0) : Math.min(this.currentCount + indx, this.maxValue - 1);
             this.willCounterMove = false;
+            this.currentCount === 0 ? document.getElementById(this.buttonLabel[0]).style.display = 'none' : document.getElementById(this.buttonLabel[0]).style.display = 'inline-block';
+            if (this.currentCount === this.maxValue - 1) {
+                document.getElementById(this.buttonLabel[0]).style.display = 'none';
+                document.getElementById(this.buttonLabel[1]).style.display = 'none';
+                document.getElementById(this.buttonLabel[2]).style.display = 'inline-block';
+            }
             this.action(this.currentCount, e.target.innerText);
         }
         return element;
